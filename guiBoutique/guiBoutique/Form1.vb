e030a35cafd509c6c0d180635f35d7456038c6ac
Hamza Mehri <azizelmehri@gmail.com>
2012-12-21 18:06:30 +0100

    Just the Fist Gui
    nabbdew juste bil Boutique

--- guiBoutique/guiBoutique/Form1.vb
+++ guiBoutique/guiBoutique/Form1.vb
@@ -3,15 +3,6 @@
 
    
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        If login.Text = "admin" And mdp.Text = "admin" Then
-
-
-
-            Form2.ShowDialog()
-
-        Else : MsgBox("Mot de passe incorrecte")
-        End If
-
-
+        
     End Sub
 End Class
