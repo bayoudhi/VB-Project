3f93a80c4e49837ccb1cb1d2d56c3ea91cea9243
Hamza Mehri <azizelmehri@gmail.com>
2012-12-21 18:34:37 +0100

    add Connection

--- guiBoutique/guiBoutique/Form1.vb
+++ guiBoutique/guiBoutique/Form1.vb
@@ -1,8 +1,21 @@
-﻿Public Class Form1
+﻿Imports System.Data.OleDb
+Public Class Form1
 
+    Dim connection As New OleDbConnection("Provider='Microsoft.Jet.OLEDB.4.0';Data Source='Stock.mdb'")
 
-   
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        
+
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            connection.Open()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+
 End Class
